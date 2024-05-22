7e8a4bbf19b21f2165683eb864c0d7808b186bb6
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-05-22 22:04:28 +0300

    upd

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+// import feedbackType from "../Feedback/feedbackType.json";
+// import Options from "../Options/Options";
+
+// const Feedback = ({ feedbackType }) => {
+//   return (
+//     <>
+//       <div>
+//         <p>Good: {feedbackType}</p>
+//         <p>Neutral: {feedbackType}</p>
+//         <p>Bad: {feedbackType}</p>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Feedback;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+// import feedbackType from "../Feedback/feedbackType.json";
+// import Feedback from "../Feedback/Feedback";
+
+// const Options = () => {
+//   return (
+//     <div>
+//       <button onClick={updateFeedback}>good</button>
+//     </div>
+//   );
+// };
+
+// export default Options;
