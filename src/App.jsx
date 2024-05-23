0fbcb3da4e188e690fbb1975f67a94acbc580eef
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-05-23 18:13:35 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-import "./App.css";
-import { useState } from "react";
+import Description from "./components/Description/Description";
 
-const App = () => {
-  // Ініціалізація стану для типів відгуків
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  // Функції для оновлення стану
-  const handleGoodFeedback = () =>
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      good: prevFeedback.good + 1,
-    }));
-  const handleNeutralFeedback = () =>
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      neutral: prevFeedback.neutral + 1,
-    }));
-  const handleBadFeedback = () =>
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      bad: prevFeedback.bad + 1,
-    }));
-
+function App() {
   return (
     <div>
-      <h1>Feedback</h1>
-      <button onClick={handleGoodFeedback}>Good</button>
-      <button onClick={handleNeutralFeedback}>Neutral</button>
-      <button onClick={handleBadFeedback}>Bad</button>
-
-      <h2>Statistics</h2>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
+      <Description />
     </div>
   );
-};
+}
 
 export default App;
